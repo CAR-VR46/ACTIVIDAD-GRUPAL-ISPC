30833e8e5d3be216b2f30585e9d234a66a642d82
Carla Loto <carlaloto89@gmail.com>
2022-08-14 23:11:29 -0300

    ejercicio.py

---
+++ ejercicio_listas.py
@@ -0,0 +1,33 @@
+# Valoracion final - Modulo Programador.
+# Escribir el código en Python que permita realizar lo siguiente:
+# 6)       Crear una tupla en Python con el nombre de “Historial2” la cual contenga los siguientes valores 23500, 5960, 2300, 10200, 8900 Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Frida”. Calcular el monto total gastado a lo largo del tiempo por atención de “Frida”.
+tupla_historial2=(23500, 5960, 2300, 10200, 8900)
+for element in tupla_historial2:
+    print(element)
+    
+print("Gastos Totales Frida")
+
+a, b, c, d, e=tupla_historial2
+print(a+b+c+d+e)
+
+# Crear una función que cuente cuantos gastos fueron superiores a 5000 mostrando  el número calculado en pantalla.
+print("convertir mi tupla en lista")
+lista_historial2= list (tupla_historial2)
+print(lista_historial2)
+
+g=5000
+
+def gastos_sup():
+    lista_historial2[23500, 5960, 2300, 10200, 8900]
+    
+    g= 5000
+    
+    print ("The list : " + str(lista_historial2)) 
+  
+count = 0
+for i in lista_historial2 : 
+    if i > g : 
+        count = count + 1
+  
+print ("The numbers greater than 5000 : " + str(count)) 
+
